7037d8858f1d4a1066b0eb32e99da3b74923cc84
Mateleone <uj22il@brocku.ca>
2025-02-01 17:24:38 -0500

    Finished Week 4

--- Prototype2/script.js
+++ Prototype2/script.js
@@ -2,9 +2,8 @@ console.log("Hello World")
 console.log("I am excited for use to make our first 3D scene.")
 
 import * as THREE from "three"
-import * as dat from "lil-gui"
 import {OrbitControls } from "OrbitControls"
-
+import * as dat from "lil-gui"
 
 
 /**SETUP */
@@ -36,7 +35,7 @@ const camera = new THREE.PerspectiveCamera(
 )
 
 scene.add(camera)
-camera.position.set(0,0,5)
+camera.position.set(-1,2,5)
 //Renderer
 
 const renderer = new THREE.WebGLRenderer({
@@ -51,14 +50,66 @@ controls.enableDamping = true
 
 //Meshes
 //testSphere
-const sphereGeometry = new THREE.SphereGeometry(1)
+const sphereGeometry = new THREE.TorusKnotGeometry(1)
 const sphereMaterial = new THREE.MeshNormalMaterial()
 const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
 scene.add(testSphere)
 
+//Plane
+const planeGeometry = new THREE.PlaneGeometry(10, 10, 20, 20)
+const planeMaterial = new THREE.MeshBasicMaterial({
+    color: new THREE.Color('White'),
+    side: THREE.DoubleSide,
+    wireframe: true
+})
+const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+plane.rotation.x = Math.PI * 0.5
+
+scene.add(plane)
+
 //UI
 const ui = new dat.GUI()
+
+//Ui object
+const uiObject = {
+    speed:1,
+    distance:1,
+    rotationSpeed: 0.5
+}
+    
+
+//TestSPehere UI
+const sphereFolder = ui.addFolder('Sphere')
+
+sphereFolder
+    .add(uiObject, 'speed')
+    .min(0.1)
+    .max(10)
+    .step(0.5)
+    .name('Speed')
+
+sphereFolder
+    .add(uiObject, 'distance')
+    .min(0.1)
+    .max(10)
+    .step(0.5)
+    .name('Distance')
+
+sphereFolder
+    .add(uiObject, 'rotationSpeed')
+    .min(0.01)
+    .max(1)
+    .step(0.01)
+    .name('Rotation Speed');
+
+//plane ui
+const planeFolder = ui.addFolder('Plane')
+
+planeFolder
+    .add(planeMaterial, 'wireframe')
+    .name("Toggle Wireframe")
+
 
 
 /**
@@ -74,6 +125,13 @@ const animation = () =>
     // Return elasped Time
     const elapsedTime = clock.getElapsedTime()
     
+    //Animate Sphere
+    testSphere.position.y = Math.sin(elapsedTime * uiObject.speed) * uiObject.distance
+
+    // Update rotation logic in the animation loop
+    testSphere.rotation.x += uiObject.rotationSpeed;
+    testSphere.rotation.y += uiObject.rotationSpeed;
+
     //Update OrbitControls
     controls.update()
 
